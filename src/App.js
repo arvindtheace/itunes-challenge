49d7ca5dd16dc31d131cb3077f93b13cb808c5f3
arvind <arvind@simility.com>
2018-11-24 14:56:28 +0530

    added basic filtering logic

--- src/App.js
+++ src/App.js
@@ -3,25 +3,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import './App.css';
 import Song from './Song';
+import { debounce } from 'lodash';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      data: {}
+      data: {},
+      filteredList: []
     }
+    this.debouncedChange = debounce((e) => {
+      this.filterList(e);
+    }, 500)
   }
   componentDidMount() {
-    fetch('https://itunes.apple.com/us/rss/topalbums/limit=5/json')
+    fetch('https://itunes.apple.com/us/rss/topalbums/limit=100/json')
       .then(res => res.json())
       .then(json => {
-        this.setState({ data: json.feed })
+        this.setState({ data: json.feed, filteredList: json.feed.entry })
       })
   }
+  onChange = (e) => {
+    e.persist();
+    this.debouncedChange(e);
+  };
+  filterList = (e) => {
+    let list = this.state.data.entry;
+    let searchString = e.target.value.toLowerCase();
+    let filteredList;
+    if(searchString) {
+      filteredList = list.filter((songInfoObj) => {
+        console.log(songInfoObj);
+        let title = songInfoObj["im:name"].label.toLowerCase();
+        return title.includes(searchString);
+      })
+    }
+    else {
+      filteredList = list;
+    }
+    this.setState({filteredList});
+  }
   render() {
-    if (!this.state.data.entry) return (<div></div>);
-    console.log(this.state.data.entry);
-    let songsList = this.state.data.entry.map((song,index) => {
+    if (!this.state.filteredList) return (<div></div>);
+    let songsList = this.state.filteredList.map((song, index) => {
       return (
         <div key={index} className="col-sm-3">
           <Song data={song} />
@@ -30,8 +54,9 @@ class App extends Component {
     })
     return (
       <div className="container">
+        <input className="searchbar" type="text" name="search" placeholder="Search..." onChange={this.onChange} />
         <div className="row">
-            {songsList}
+          {songsList.length > 0 ? songsList : <div className="emptyMsg">No songs found!</div>}
         </div>
       </div>
     );
